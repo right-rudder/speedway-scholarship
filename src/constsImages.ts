b0d0b4329dd6a77b54995ee4df84e2d1af153d5d
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-05-14 16:51:24 -0300

    Add new image assets

--- src/constsImages.ts
+++ src/constsImages.ts
@@ -2,8 +2,8 @@ export const PLACEHOLDER_PERSON = { imagePath: "/src/assets/placeholder-person.j
 export const PLACEHOLDER = { imagePath: "/src/assets/placeholder.jpg", imageAlt: " ", };
 export const RRM_TEXT_BLACK = { imagePath: "/src/assets/RRM-hor-textBlack-bgTrans-150.webp", imageAlt: " ", };
 export const RRM_R_SYMBOL = { imagePath: "/src/assets/RRM-R-symbol-474.webp", imageAlt: " ", };
-export const LOGO_WRITTEN = { imagePath: "/public/speedway-flight-training-written-logo.png", imageAlt: " ", };
-export const LOGO = { imagePath: "/public/speedway-flight-training-hampton-georgia-logo-red.png", imageAlt: " ", };
+export const LOGO_WRITTEN = { imagePath: "/speedway-flight-training-written-logo.png", imageAlt: " ", };
+export const LOGO = { imagePath: "/speedway-flight-training-hampton-georgia-logo-red.png", imageAlt: " ", };
 
 export const BLAKE_GUIDRY = { imagePath: "/src/assets/blake-guidry-UOJ6vz2khrY-unsplash.jpg", imageAlt: "Photo by Blake Guidry" };
 export const JORGEN_HENDRICKSEN = { imagePath: "/src/assets/jorgen-hendriksen-UfVy7UjQ_rA-unsplash.jpg", imageAlt: "Photo by Jørgen Hendriksen" };
@@ -13,3 +13,6 @@ export const RAYYU_MALDIVES = { imagePath: "/src/assets/rayyu-maldives-vZ5Tk3cc5
 export const RRM_HORIZONTAL_LOGO = { imagePath: "/src/assets/RRM-hor-textBlack-bgTrans-150.webp", imageAlt: "RRM horizontal logo" };
 export const RRM_SYMBOL = { imagePath: "/src/assets/RRM-R-symbol-474.webp", imageAlt: "RRM symbol logo" };
 export const WESTWIND_AIR_SERVICE = { imagePath: "/src/assets/westwind-air-service-_TUvJQS9Aoo-unsplash.jpg", imageAlt: "Photo by Westwind Air Service" };
+
+export const MENTOR_PILOT_SHAKING_HANDS_WITH_STUDENT = {imagePath:"/src/assets/A_mentor_pilot_shaking_hands_with_a_student_while_holding_scholarship_paperwork,_with_a_plane_in_the_background.png" , imageAlt: "A mentor pilot shaking hands with a student while holding scholarship paperwork, with a plane in the background"};
+export const PILOT_STUDENT_DOING_CHECKLIST_ON_PLANE = {imagePath:"/src/assets/A_close-up_of_a_student_pilot_in_a_crisp_white_shirtepaulets_performing_a_pre-flight_inspection_on_a_plane,_checklist_in_hand_2.png" , imageAlt: "A close-up of a student pilot in a crisp white shirtepaulets performing a pre-flight inspection on a plane, checklist in hand"};
