47ea1a4463769d7e15a55cfa64635a6ef95c3a40
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-06-05 12:04:26 -0300

    Update consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,19 @@
 // CHANGE Default title
-export const SITE_TITLE = "Airplane and Helicopter Flight Training in ... , STATE | SCHOOLNAME";
+export const SITE_TITLE = "Speedway Flight Training Scholarship Fund. | Airplane Flight Training Scholarship Foundation in Hampton, Georgia";
 
 // CHANGE default meta description -> should only be used in homepage technically, but is there for a fallback in case a page is missed
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION = "Discover premier airplane flight training at Speedway Flight Training Scholarship Fund in Hampton, Georgia. Offering comprehensive pilot programs and scholarships.";
 
 //SEO tip -> keyword + city 1
 //Should have between 5-10 different localities to mix and match keywords
 //typical keywords = how to become a pilot, flight school near, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot
 // CHANGE default keywords -> should only be used in homepage technically, but is there for a fallback in case a page is missed
-export const KEYWORDS = "";
+export const KEYWORDS = "flight school Hampton GA, pilot training Hampton, flight scholarships Georgia, Speedway Flight Training, learn to fly Georgia, private pilot Hampton, commercial pilot Hampton";
 
 export const OG_IMAGE = "/Speedway-Scholarship-Fund-OG-image.png";
 
-export const GEO_LATITUDE = 12.12345678901234;
-export const GEO_LONGITUDE = -12.12345678901234;
+// export const GEO_LATITUDE = 12.12345678901234;
+// export const GEO_LONGITUDE = -12.12345678901234;
 
 export const LOCATIONS = [
   {
@@ -30,8 +30,8 @@ export const LOCATIONS = [
 // CHANGE client business info
 
 export const COMPANY_NAME = "Speedway Flight Training Scholarship Fund.";
-export const COMPANY_NAME_CAPS = "SPEEDWAY FLIGHT TRAINING";
-export const COMPANY_NICKNAME = "Speedway";
+export const COMPANY_NAME_CAPS = "SPEEDWAY FLIGHT TRAINING FUND.";
+export const COMPANY_NICKNAME = "Speedway Fund.";
 export const LOGO_PRIMARY = "/speedway-flight-training-written-logo.png";
 export const LOGO_SECONDARY = "/speedway-flight-training-written-logo.png";
 export const PHONE_NUMBER = "(678) 944-7379";
@@ -53,4 +53,3 @@ export const TWITTER_URL = null;
 // Pages Data
 
 export const BANNER_TITLE = `Dream • Train • Fly  |  Speedway Flight Training Scholarship Fund.`;
-
